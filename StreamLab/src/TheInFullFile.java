8415e66523d7bdbb79eb8dba3bbcd74fef1c7965
Nadim-Mahmud <mahmudnadim16@gmail.com>
2018-10-07 15:23:14 +0600

    19.2 completed

---
+++ StreamLab/src/TheInFullFile.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class TheInFullFile {
+	
+public long counter(){
+		
+		long count=0;
+		
+		try {
+			
+			Stream<String> words = Files.lines(Paths.get("input.txt"));
+			
+		    count = words
+				.filter((w) -> w.equals("the"))
+				.count();
+		}
+		catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+		return count;
+	}
+
+}
--- StreamLab/src/main.java
+++ StreamLab/src/main.java
@@ -5,7 +5,8 @@ public class main {
 		
 		HowManyWordThe the = new HowManyWordThe();
 		
-		System.out.println(the.counter());
+		System.out.println("19.1 : " + the.counter());
+		System.out.println("19.2 : " + new TheInFullFile().counter());
 
 	}
 
