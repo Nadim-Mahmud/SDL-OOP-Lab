e52462c8312cc04473bdb22fba163d07d2fed32a
Nadim-Mahmud <mahmudnadim16@gmail.com>
2018-10-07 14:43:02 +0600

    shape Calculator added

---
+++ Shape Claculator/src/controller/ShapeCalcController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import view.ShapeCalc;
+
+public class ShapeCalcController {
+	
+	ShapeCalc shapeCalc;
+	
+	public ShapeCalcController(String title) {
+		
+		shapeCalc =new ShapeCalc(title);
+		shapeCalc.validate();
+		shapeCalc.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		
+		new ShapeCalcController("Shape Calculator");
+		
+	}
+
+}
